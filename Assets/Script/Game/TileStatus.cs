dc7c1aa31d1d812ebe09ba4e9bcaf5a042175ee7
정 락준 <jrj325@hanmir.com>
2014-01-24 00:19:57 +0900

    Add all thing

---
+++ Assets/Script/Game/TileStatus.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileStatus : MonoBehaviour {
+	public MainLogic.TILETYPE myType;
+	public int myX,myY;
+	public int myHp;
+	public TileStatus(int y,int x){
+		// ㅅㅐㄹㅗ ㅅㅓㄴ ㅇㅓㄴ 
+		myY = y;
+		myX = x;
+
+		myHp = 0;
+		SetHp (1);
+		
+		NewType ();
+	}
+	public void SetHp(int addHp){
+		myHp += addHp;
+	}
+	public void NewType(){
+		int t;
+		do{
+			t = (int)(Random.value * 5.0f);
+		}while(t == 5);
+		
+		switch(t){
+		case 0: myType = MainLogic.TILETYPE.Enemy; break;
+		case 1: myType = MainLogic.TILETYPE.Sword; break;
+		case 2: myType = MainLogic.TILETYPE.Wand; break;
+		case 3: myType = MainLogic.TILETYPE.Coin; break;
+		case 4: myType = MainLogic.TILETYPE.Potion; break;
+		}
+	}
+	public static bool EqualType(MainLogic.TILETYPE fType,MainLogic.TILETYPE sType){
+		if(fType == sType){
+			return true;
+		}
+		else if(fType == MainLogic.TILETYPE.Sword && sType == MainLogic.TILETYPE.Enemy){
+			return true;
+		}
+		else if (fType == MainLogic.TILETYPE.Enemy && sType == MainLogic.TILETYPE.Sword){
+			return true;
+		}
+		return false;
+	}
+}
---
+++ Assets/Script/User/UserData.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserData : MonoBehaviour {
+	private int mHp;
+	private int mAtk;
+	private int mDef;
+	private int mInt;
+	private int mCoin;
+
+	// use instance -> UserData.Instance.Hp 
+
+	private static UserData ins;
+	public static UserData Instance {
+		get { 
+			return ins; 
+		}
+	}
+	public int Hp{
+		get { return ins.mHp; }
+		set { mHp = value; }
+	}
+	public int Atk{
+		get { return mAtk; }
+		set { mAtk = value; }
+	}
+	public int Def{
+		get { return mDef; }
+		set { mDef = value; }
+	}
+	public int Int{
+		get { return mInt; }
+		set { mInt = value; }
+	}
+	public int Coin{
+		get { return mCoin; }
+		set { mCoin = value; }
+	}
+	void Start () {
+		// DataLoad
+		ins = new UserData();
+
+		//PlayerPrefs -> DataLoad and DataSave
+
+		ins.Hp = PlayerPrefs.GetInt ("Hp");
+		ins.Atk = PlayerPrefs.GetInt ("Atk");
+		ins.Def = PlayerPrefs.GetInt ("Def");
+		ins.Int = PlayerPrefs.GetInt ("Int");
+		ins.Coin = PlayerPrefs.GetInt ("Coin");
+		
+		PlayerPrefs.SetInt("Hp",20);
+		PlayerPrefs.SetInt("Atk",10);
+		PlayerPrefs.SetInt("Def",10);
+		PlayerPrefs.SetInt("Int",10);
+		PlayerPrefs.SetInt("Coin",10);
+
+		DontDestroyOnLoad(this);	
+		Application.LoadLevel(1);
+	}
+
+	void Update () {
+	
+	}
+}
