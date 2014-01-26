366c5cb66c6a2c704485d598556d670510d664f8
정 락준 <jrj325@hanmir.com>
2014-01-26 17:42:52 +0900

    1/26 rock

---
+++ Assets/Script/Game/ComboLogic.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class ComboLogic : MonoBehaviour {
+	public static int COMBO_LEN = 6;
+	GameObject[] Combo = new GameObject[COMBO_LEN];
+	MainLogic.TILETYPE[] type = new MainLogic.TILETYPE[COMBO_LEN];
+	int length;
+	int complete;
+	void Start () {
+		int i;
+		for(i=0;i<COMBO_LEN;i++){
+			Combo[i] = new GameObject("Combo"+i.ToString());
+			Combo[i].AddComponent<tk2dSprite>();
+			Combo[i].transform.parent = this.transform;
+			Combo[i].transform.localPosition = new Vector3(i*3,0,0);
+		}
+		NewComboSetting();
+	}
+
+	public void AddCombo(MainLogic.TILETYPE ntype){
+		if(ntype == type[complete]){
+			iTween.ScaleTo(Combo[complete], iTween.Hash(
+				"x", 0.5f,
+				"y", 0.5f,
+				"easeType", "easeOutQuad",
+				"time", 1,
+				"oncomplete","AddComboEnd",
+				"oncompletetarget",gameObject,
+				"oncompleteparams",Combo[complete]));
+
+			complete ++;
+		}
+	}
+	void AddComboEnd(){
+		if(complete >= length){
+			//Effect ! 
+			NewComboSetting ();
+		}
+	}
+	void NewComboSetting(){
+		int i;
+		for(i=0;i<COMBO_LEN;i++){
+			Combo[i].transform.localScale = new Vector3(0,0,0);
+		}
+		//length random ㅠㅠ ㅎㅐ
+		complete = 0;
+		do{
+			length = (int)(Random.value * ((float)(COMBO_LEN-2)))+3;
+		}while(length == COMBO_LEN+1);
+
+		int t;
+		for(i=0;i<length;i++){
+			do{
+				t = (int)(Random.value * 4.0f);
+			}while(t == 4);
+			tk2dSprite sprite = Combo[i].GetComponent<tk2dSprite>(); 
+			switch(t){
+				case 0: 
+					type[i] = MainLogic.TILETYPE.Sword;
+					sprite.SetSprite(Tile.datas,"Sword"); 
+					break;
+				case 1: 
+					type[i] = MainLogic.TILETYPE.Wand; 
+					sprite.SetSprite(Tile.datas,"Wand");
+					break;
+				case 2: 
+					type[i] = MainLogic.TILETYPE.Coin; 
+					sprite.SetSprite(Tile.datas,"Coin");
+					break;
+				case 3: 
+					type[i] = MainLogic.TILETYPE.Potion; 
+					sprite.SetSprite(Tile.datas,"Potion");
+					break;
+			}
+			Combo[i].transform.localScale = new Vector3(1,1,0);
+		}
+	}
+}
+
---
+++ Assets/Script/Game/UserText.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class UserText : MonoBehaviour {
+	GameObject Atk;
+	GameObject Int;
+	GameObject Def;
+	GameObject Hp;
+	GameObject Xien;
+	GameObject Coin;
+	void Start(){
+		Atk = GameObject.Find ("Atk Gap");
+		Int = GameObject.Find ("Int Gap");
+		Def = GameObject.Find ("Def Gap");
+		Hp = GameObject.Find ("Hp Gap");
+		Xien = GameObject.Find ("Xien Gap");
+		Coin = GameObject.Find ("Coin Gap");
+		setStat();
+	}
+	public void setStat(){
+		Atk.GetComponent<tk2dTextMesh>().text = UserData.Instance.Atk.ToString();
+		Atk.GetComponent<tk2dTextMesh>().Commit();
+		Int.GetComponent<tk2dTextMesh>().text = UserData.Instance.Int.ToString();
+		Int.GetComponent<tk2dTextMesh>().Commit();
+		Def.GetComponent<tk2dTextMesh>().text = UserData.Instance.Def.ToString();
+		Def.GetComponent<tk2dTextMesh>().Commit();
+		Hp.GetComponent<tk2dTextMesh>().text = UserData.Instance.Hp.ToString()+"/"+UserData.Instance.HpMax.ToString();
+		Hp.GetComponent<tk2dTextMesh>().Commit();
+		Xien.GetComponent<tk2dTextMesh>().text = UserData.Instance.Xien.ToString()+"/"+UserData.Instance.XienMax.ToString();
+		Xien.GetComponent<tk2dTextMesh>().Commit();
+		Coin.GetComponent<tk2dTextMesh>().text = UserData.Instance.Coin.ToString();
+		Coin.GetComponent<tk2dTextMesh>().Commit();
+	}
+	void Update(){
+
+	}
+}
